93b11a520bcb11a8b20e35e663845cee4ebaf8fd
Patryk Omiotek <patryk.omiotek@gmail.com>
2026-04-28 15:56:17 +0200

    feat: with testing-library

---
+++ src/components/Counter.spec.ts
@@ -0,0 +1,20 @@
+import { render, fireEvent, screen } from "@testing-library/vue";
+import Component from "./Counter.vue";
+
+test("increments value on click", async () => {
+  const { debug } = render(Component);
+  debug();
+
+  // screen has all queries that you can use in your tests.
+  // getByText returns the first matching node for the provided text, and
+  // throws an error if no elements match or if more than one match is found.
+  screen.getByText("Times clicked: 0");
+
+  const button = screen.getByText("increment");
+
+  // Dispatch a native click event to our button element.
+  await fireEvent.click(button);
+  await fireEvent.click(button);
+
+  screen.getByText("Times clicked: 2");
+});
